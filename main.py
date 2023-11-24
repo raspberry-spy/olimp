20e7e3f24299d04ca4fa13e19ba4c2a26c7a3ee0
nit <wisemisticaltree5@gmail.com>
2023-11-24 20:53:20 +0200

    a

--- main.py
+++ main.py
@@ -107,6 +107,40 @@ def cnn_digits_predict(model, image_file, tf):
 
     # Получение числа из изображения с помощью модели
     return np.argmax(model.predict([img], verbose=0))
+def train4():
+    import tensorflow as tf
+    from tensorflow.keras.datasets import mnist
+    # Загрузка и нормализация датасета
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    # Функция сохранения весов в файл
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath='cp/cp.ckpt',
+                                                     save_weights_only=True,
+                                                     verbose=1)
+
+    # Задание формы модели (стек слоев)
+    model = tf.keras.models.Sequential([
+        # первый слой сети - преобразует формат изображений в одномерный массив 28 * 28 = 784 пикселей
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        # второй слой = 128 узлов (нейронов)
+        tf.keras.layers.Dense(128, activation='relu'),
+        # третий слой (выходной) = 10 узлов (нейронов)
+        tf.keras.layers.Dense(10)
+    ])
+
+    model.compile(
+        # Оптимизатор обучения модели
+        optimizer=tf.keras.optimizers.Adam(0.001),
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+    )
+
+    model.fit(
+        x_train, # Датасет с изображениями
+        y_train, # Датасет с расшифровкой изображений
+        epochs=50,
+        callbacks=[cp_callback] # Сохранение весов в файл
+    )
 
 
 def prog4():
@@ -134,7 +168,7 @@ def prog4():
 
 # ---- ИНТЕРФЕЙС_ПРОВЕРКИ_ЗАДАНИЙ ----
 while True:
-    uinput = input("Введите номер задания (1-4) или нажмите 5 для выхода\n")
+    uinput = input("Введите номер задания (1-4), нажмите 0 для выхода или 5 для переобучения модели 4 задания\n")
     if uinput == "1":
         prog1()
     elif uinput == "2":
@@ -144,5 +178,7 @@ while True:
     elif uinput == "4":
         prog4()
     elif uinput == "5":
+        train4()
+    elif uinput == "0":
         print("Выход из программы")
         break
